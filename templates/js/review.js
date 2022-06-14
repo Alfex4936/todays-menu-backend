81257bed96bc9c44ff8f7fc1634c982f6f21afdb
seok <ikr@kakao.com>
2022-06-14 13:08:38 +0900

    Review: HTML 별점 평가

--- templates/js/review.js
+++ templates/js/review.js
@@ -1,37 +1,5 @@
-// star rating init
-$(".rateYo").each(function (e) {
-  var ChngRatevaluesEn = {
-    1: "bad",
-    2: "poor",
-    3: "ok",
-    4: "good",
-    5: "super",
-  };
-  var language = "english";
-  var rating = $(this).attr("data-rating");
-  $(this).rateYo({
-    onSet: function (rating) {
-      $(this).next().val(ChngRatevaluesEn[rating]);
-      ratingFunc(rating, $(this).next().next().val());
-    },
-    rating: rating,
-    starWidth: "20px",
-    numStars: 5,
-    fullStar: true,
-    normalFill: "#A0A0A0",
-    spacing: "5px",
-    precision: 2,
-    // rtl: true,
-    // readOnly: true,
-  });
-});
+const drawStar = target => {
+  document.querySelector(`.star span`).style.width = `${target.value * 10}%`;
 
-function ratingFunc(rating, bookid, lang) {
-  debugger;
-  if (lang != null) {
-    language = lang;
-  }
-
-  alert("selected star Rate is " + rating + " And Book id is " + bookid);
-  //database call goes here!!!
-}
+  console.log(document.getElementById("star_rate").value);
+};
